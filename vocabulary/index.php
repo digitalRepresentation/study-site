3f34ab8d883781b399ec5eeaa85bb4a45606c593
jinyoung <kinemura@naver.com>
2022-05-17 22:56:41 +0900

    add 単語帳リストdesign

---
+++ vocabulary/index.php
@@ -0,0 +1,8 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/lib/config.php");
+    // アカウント作成ページのコントローラー。
+    
+
+    // PCバージョン
+    include DOCUMENT_ROOT . "/vocabulary/index_pc.html";
+?>
