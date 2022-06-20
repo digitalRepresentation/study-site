952a3968aca9b05342c7d7f954e13b94a32b1b1b
jinyoung <kinemura@naver.com>
2022-06-20 18:21:14 +0900

    ---漏れ対応

--- lib/DB.php
+++ lib/DB.php
@@ -1,9 +1,9 @@
 <?php
 // DB設定
-define("DB_HOST_NAME", "");
-define("DB_ID", "");
-define("DB_PASSWORD", "");
-define("DB_DB_NAME", "");
+define("DB_HOST_NAME", "localhost");
+define("DB_ID", "root");
+define("DB_PASSWORD", "root");
+define("DB_DB_NAME", "study");
 
 try {
     $pdo = new PDO('mysql:dbname=' . DB_DB_NAME . ';host=' . DB_HOST_NAME . ';charset=UTF8;', DB_ID, DB_PASSWORD);
